b835510c63aedc5ca75135a6c193fc2b8142489a
Your Name <you@example.com>
2011-08-23 23:59:49 +0200

    primeira subida. Probando un Chart

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProbaChart::Application.config.secret_token = '78c9f24b2f74f228f8db9b40880389c38ab70710abcafc638a32f0a74ab75f449393437c4bfcdaf497fc6bd9b77825d8490a2e162662b2059933e688f001cf6a'
